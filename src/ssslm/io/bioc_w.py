20c38db5c66dbda1b014d5ebb0d05f116d600de7
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-12-23 09:06:29 -0500

    Refactor

--- src/ssslm/io/bioc_w.py
+++ src/ssslm/io/bioc_w.py
@@ -4,23 +4,27 @@ from __future__ import annotations
 
 from collections.abc import Iterable
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, TypedDict
 
+import bioc.pubtator
+import pandas as pd
+import pystow
+from bioc.pubtator import PubTator
 from curies import NamableReference, NamedReference
 from curies.vocabulary import abstract_section, title_section
 from pydantic import BaseModel
+from tqdm import tqdm
 
 from ssslm.ner import Annotation, Match
 
 if TYPE_CHECKING:
-    from bioc.pubtator import PubTator
-
     from ssslm import Grounder
 
 __all__ = [
     "Document",
-    "parse_pubtator",
 ]
+
+MODULE = pystow.module("bioc")
 
 
 class Document(BaseModel):
@@ -30,22 +34,6 @@ class Document(BaseModel):
     text: str
     document_part: NamableReference
     annotations: list[Annotation]
-
-
-def parse_pubtator(
-    path: str | Path, *, prefix: str | None = None, confidence: float | None = None
-) -> Iterable[Document]:
-    """Parse a BioC PubTator file."""
-    import bioc.pubtator
-
-    if confidence is None:
-        confidence = 1.0
-    if prefix is None:
-        prefix = "mesh"
-
-    with Path(path).expanduser().open() as file:
-        for pubtator in bioc.pubtator.iterparse(file):
-            yield from _pubtator_to_documents(pubtator, confidence=confidence, prefix=prefix)
 
 
 def _pubtator_to_documents(
@@ -61,7 +49,7 @@ def _pubtator_to_documents(
 
     title_len = len(pubtator.title)
     for annotation in pubtator.annotations:
-        if prefix:
+        if prefix is not None:
             annotation_reference = NamableReference(prefix=prefix, identifier=annotation.id)
         else:
             annotation_reference = NamableReference.from_curie(annotation.id)
@@ -101,32 +89,152 @@ def _pubtator_to_documents(
     return [d1, d2]
 
 
-def evaluate_recall(
-    *, grounder: Grounder | None = None, path: str | Path, prefix: str | None = None
+def evaluate_path(
+    path: str | Path,
+    *,
+    prefix: str | None = None,
+    grounder: Grounder | None = None,
+    confidence: float | None = None,
+) -> None:
+    import bioc.pubtator
+
+    with Path(path).expanduser().open() as file:
+        evaluate(
+            bioc.pubtator.iterparse(file),
+            grounder=grounder,
+            prefix=prefix,
+            confidence=confidence,
+        )
+
+
+def evaluate(
+    pubtators: Iterable[PubTator],
+    *,
+    grounder: Grounder | None = None,
+    confidence: float | None = None,
+    prefix: str | None = None,
 ) -> None:
     """Evaluate a BioC file against a grounder."""
-    import click
+    import seaborn as sns
 
     if grounder is None:
         import pyobo
 
         grounder = pyobo.get_grounder("mesh")
 
-    for document in parse_pubtator(path, prefix=prefix):
-        result = _compare(
-            document.annotations,
-            grounder.annotate(document.text),
-        )
-        click.echo(f"{document.reference.curie}: {result:.2f}")
+    precisions = []
+    for pubtator in tqdm(pubtators, unit_scale=True):
+        for document in _pubtator_to_documents(pubtator, prefix=prefix, confidence=confidence):
+            result = _compare(
+                document.annotations,
+                grounder.annotate(document.text),
+            )
+            if result is not None:
+                precisions.append(result)
+            else:
+                tqdm.write(f"{document.reference.curie}: NaN (no ground truth)")
+
+    df = pd.DataFrame(precisions)
+    g = sns.displot(
+        data=df.melt(var_name="variable", value_name="value"),
+        x="value",
+        col="variable",
+        bins=30,
+        height=4,
+        aspect=1.7,
+    )
+    for ax in g.axes.flatten():
+        ax.set_yscale("log")
+    g.figure.savefig("/Users/cthoyt/Desktop/results.png", dpi=400)
 
 
-def _compare(expected: list[Annotation], actual: list[Annotation]) -> float:
+class Evaluation(TypedDict):
+    """A collection of metrics."""
+
+    offsets_recall: float | None
+    entities_recall: float | None
+
+
+def _compare(expected: list[Annotation], actual: list[Annotation]) -> Evaluation | None:
     expected_offsets = {(a.start, a.end) for a in expected}
     actual_offsets = {(a.start, a.end) for a in actual}
-    intersection = actual_offsets.intersection(expected_offsets)
-    recall = len(intersection) / len(expected_offsets)
-    return recall
+    offsets_intersection = actual_offsets.intersection(expected_offsets)
+
+    expected_entities = {a.reference.curie for a in expected}
+    actual_entities = {a.reference.curie for a in actual}
+    entities_intersection = actual_entities.intersection(expected_entities)
+
+    if not expected_offsets:
+        offsets_recall = None
+    else:
+        offsets_recall = len(offsets_intersection) / len(expected_offsets)
+
+    if not expected_entities:
+        entities_recall = None
+    else:
+        entities_recall = len(entities_intersection) / len(expected_entities)
+
+    rv = Evaluation(offsets_recall=offsets_recall, entities_recall=entities_recall)
+    if all(v is None for v in rv.values()):
+        return None
+    return rv
+
+
+BC5_CDR_URL = "https://github.com/JHnlp/BioCreative-V-CDR-Corpus/raw/refs/heads/master/CDR_Data.zip"
+BC5_CDR_EVALUATION_URL = "https://github.com/JHnlp/BioCreative-V-CDR-Corpus/raw/refs/heads/master/BC5CDR_Evaluation-0.0.3.zip"
+
+
+def load_bc5_cdr_development() -> list[PubTator]:
+    # annotated with MeSH LUIDs
+    return _load_bc5("CDR.Corpus.v010516/CDR_DevelopmentSet.PubTator.txt")
+
+
+def load_bc5_cdr_test() -> list[PubTator]:
+    # annotated with MeSH LUIDs
+    return _load_bc5("CDR.Corpus.v010516/CCDR_TestSet.PubTator.txt")
+
+
+def load_bc5_cdr_training() -> list[PubTator]:
+    # annotated with MeSH LUIDs
+    return _load_bc5("CDR.Corpus.v010516/CDR_TrainingSet.PubTator.txt")
+
+
+def load_bc5_dorm_test() -> list[PubTator]:
+    # this one is annotated with CURIEs from MESH and CHEBI
+    return _load_bc5("DNorm.TestSet/TestSet.DNorm.PubTator.txt")
+
+
+def load_bc5_tmchem_test() -> list[PubTator]:
+    # this one is annotated with CURIEs from MESH and CHEBI
+    return _load_bc5("tmChem.TestSet/TestSet.tmChem.PubTator.txt")
+
+
+def _load_bc5(inner_path: str) -> list[PubTator]:
+    with MODULE.ensure_open_zip(
+        url=BC5_CDR_URL, inner_path="CDR_Data/" + inner_path, mode="rt"
+    ) as zf:
+        return bioc.pubtator.load(zf)
+
+
+def load_bc5_evaluation_sample() -> list[PubTator]:
+    # annotated with MeSH LUIDs
+    return _load_bc5_evaluation("data/gold/CDR_sample.gold.PubTator")
+
+
+def load_bc5_evaluation_cid() -> list[PubTator]:
+    # this one is annotated with CURIEs from MESH and CHEBI
+    return _load_bc5_evaluation("data/test/CDR_sample.test.CID.PubTator")
+
+
+def load_bc5_evaluation_dner() -> list[PubTator]:
+    # this one is annotated with CURIEs from MESH
+    return _load_bc5_evaluation("data/test/CDR_sample.test.DNER.PubTator")
+
+
+def _load_bc5_evaluation(inner_path: str) -> list[PubTator]:
+    with MODULE.ensure_open_zip(url=BC5_CDR_EVALUATION_URL, inner_path=inner_path, mode="rt") as zf:
+        return bioc.pubtator.load(zf)
 
 
 if __name__ == "__main__":
-    evaluate_recall(path="/Users/cthoyt/dev/BioCreative-V-CDR-Corpus/CDR_sample.txt")
+    evaluate(load_bc5_dorm_test())
